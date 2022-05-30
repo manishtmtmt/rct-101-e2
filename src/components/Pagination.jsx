65f521165d011fe558a2b0caabe7d2da2207b467
manishtmtmt <manisht.mt.mt@gmail.com>
2022-05-30 16:30:43 +0530

    pagination part done

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,20 +1,24 @@
 import { Button, ButtonGroup, Select } from "@chakra-ui/react";
 import React from "react";
 
-const Pagination = () => {
+const Pagination = ({ previous, next, onChange, first, last }) => {
   
 
   return (
     <ButtonGroup>
-      <Button data-cy="pagination-first-button">First</Button>
-      <Button data-cy="pagination-previous-button">Previous</Button>
-      <Select data-cy="pagination-limit-select">
-        <option data-cy="pagination-limit-3">3</option>
-        <option data-cy="pagination-limit-6">6</option>
-        <option data-cy="pagination-limit-9">9</option>
+      <Button data-cy="pagination-first-button" onClick={first}>First</Button>
+      <Button data-cy="pagination-previous-button" onClick={previous}>
+        Previous
+      </Button>
+      <Select data-cy="pagination-limit-select" onChange={onChange}>
+        <option value={3} data-cy="pagination-limit-3">3</option>
+        <option value={6} data-cy="pagination-limit-6">6</option>
+        <option value={9} data-cy="pagination-limit-9">9</option>
       </Select>
-      <Button data-cy="pagination-next-button">Next</Button>
-      <Button data-cy="pagination-last-button">Last</Button>
+      <Button data-cy="pagination-next-button" onClick={next}>
+        Next
+      </Button>
+      <Button data-cy="pagination-last-button" onClick={last}>Last</Button>
     </ButtonGroup>
   );
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,38 +1,66 @@
 import React, { useEffect, useState } from "react";
-import AddProduct from "./AddProduct"
-import Product from "./Product"
-import Pagination from "./Pagination"
-import axios from "axios"
+import AddProduct from "./AddProduct";
+import Product from "./Product";
+import Pagination from "./Pagination";
+import axios from "axios";
 import { Flex, Grid } from "@chakra-ui/react";
 
 const Products = () => {
-  const [products, setProducts] = useState([])
-  const [page, setPage] = useState(1)
-  const [limit, setLimit] = useState(3)
-  const [totalCount, setTotalCount] = useState(0)
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(3);
+  const [totalCount, setTotalCount] = useState(0);
 
-  useEffect(()=>{
+  useEffect(() => {
     axios
-    .get(`http://localhost:8080/products?_page=${page}&_limit=${limit}`)
-    .then((r)=>{
-      // console.log(r.data)
-      setProducts(r.data)
-      
-    })
-  },[])
-  console.log(products)
+      .get(`http://localhost:8080/products?_page=${page}&_limit=${limit}`)
+      .then((r) => {
+        // console.log(r.data)
+        setProducts(r.data);
+        setTotalCount(Number(r.headers["x-total-count"]));
+      });
+  }, [page, limit]);
+  console.log(products);
 
   const addProduct = (newProduct) => {
+    // addProduct
+  };
 
+  const next = () => {
+    setPage(page + 1);
+  };
+
+  const previous = () => {
+    setPage(page - 1);
+  };
+
+  const onChange = ({ target }) => {
+    setLimit(Number(target.value));
+  };
+
+  const first = () => {
+    setPage(1)
+  }
+
+  const last = () => {
+    setPage(Math.floor(totalCount / limit))
   }
 
   return (
     <Flex>
-      <AddProduct  />
-      <Grid>{products.map((product)=>(
-        <Product key={product.id} product={product} />
-      ))}</Grid>
-      <Pagination />
+      <AddProduct addProduct={addProduct} />
+      <Grid>
+        {products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </Grid>
+      <Pagination
+        next={next}
+        previous={previous}
+        onChange={onChange}
+        first={first}
+        last={last}
+      />
     </Flex>
   );
 };
